00d4c9c839d6386cb9e6e0906a24e8b62cf42957
ibrahim abdalrhman <59165733+ibrahimabdalrhman@users.noreply.github.com>
2024-02-17 14:35:00 +0200

    allowTo funcation رTo control the powers of admin,managers,users

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,29 +2,26 @@ const { User } = require("../models");
 const asyncHandler = require("express-async-handler");
 const ApiError = require("../utils/apiError");
 const cloudinary = require("../utils/cloudinary");
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const createToken = (payload) =>
   jwt.sign({ UserId: payload }, process.env.JWT_KEY, {
     expiresIn: process.env.JWT_EXPIRE_TIME,
   });
 
-
 exports.signup = asyncHandler(async (req, res) => {
-
   const user = await User.create(req.body);
 
   res.status(201).json(user);
 });
 
 exports.login = asyncHandler(async (req, res, next) => {
-
   const user = await User.findOne({ where: { email: req.body.email } });
   if (!user) {
     return next(new ApiError("incorrect email or password"), 401);
   }
-  const correct = await bcrypt.compare(req.body.password, user.password,);
+  const correct = await bcrypt.compare(req.body.password, user.password);
   if (!correct) {
     return next(new ApiError("incorrect email or password"), 401);
   }
@@ -32,11 +29,11 @@ exports.login = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     msg: `welcome ${user.name}`,
     user,
-    token
+    token,
   });
 });
 
-exports.protect = asyncHandler(async (req, res, next) => { 
+exports.protect = asyncHandler(async (req, res, next) => {
   if (!req.headers.authorization) {
     return next(new ApiError("you must login to access this route ", 401));
   }
@@ -64,16 +61,17 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-
 exports.profileImage = asyncHandler(async (req, res, next) => {
-  
   if (req.files && req.files.profileImage) {
     const profileImage = req.files.profileImage;
-    const uploadRes = await cloudinary.uploader.upload(profileImage.tempFilePath, {
-      public_id: `${Date.now()}`,
-      resource_type: "image",
-      folder: "ecommerce-mysql/user",
-    });
+    const uploadRes = await cloudinary.uploader.upload(
+      profileImage.tempFilePath,
+      {
+        public_id: `${Date.now()}`,
+        resource_type: "image",
+        folder: "ecommerce-mysql/user",
+      }
+    );
     req.body.profileImage = uploadRes.url;
   }
   const user = await User.findByPk(req.user.id);
@@ -82,5 +80,13 @@ exports.profileImage = asyncHandler(async (req, res, next) => {
   res.status(200).json(user);
 });
 
-
+exports.allowTo = (...roles) =>
+  asyncHandler(async (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ApiError("You are not allowed to access this route", 403)
+      );
+    }
+    next();
+  });
 
